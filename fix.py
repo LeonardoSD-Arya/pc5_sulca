178e5142915b87be4c35b3adad8bf6c2ee77ba46
LeonardoSD-Arya <98826629+LeonardoSD-Arya@users.noreply.github.com>
2022-07-25 22:38:43 -0500

    Commit yaml 6

--- fix.py
+++
@@ -1,9 +0,0 @@
-with open('build/index.html', 'r') as file :
-  filedata = file.read()
-
-# Replace the target string
-filedata = filedata.replace('/static/', '/pc5_sulca/static/')
-
-# Write the file out again
-with open('build/index.html', 'w') as file:
-  file.write(filedata)
